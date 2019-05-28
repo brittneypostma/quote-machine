dc2fe5126a3124fdded1b3435c488de35778fcf4
Brittney Postma <brittney.postma@hotmail.com>
2019-05-28 09:32:38 -0400

    *todolistv1*

---
+++ todolist-v1/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const app = express()
+
+let items = []
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    const today = new Date();
+
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+
+    const day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {
+        kindOfDay: day,
+        newListItems: items
+    });
+});
+
+app.post("/", function (req, res) {
+    let item = req.body.newItem;
+
+    items.push(item)
+
+    res.redirect("/");
+});
+
+app.listen(3000, function () {
+    console.log("Server running on port 3000.")
+});
